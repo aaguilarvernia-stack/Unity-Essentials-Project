875b4b63c0b026b5d7eca04bcc847d8f9009b417
Alexagver <al417991@uji.es>
2025-09-24 22:13:11 +0200

    Actu 3

---
+++ Assets/CambiadorEscena.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiadorEscena : MonoBehaviour
+{
+    public bool pasarNivel;
+    public int indiceNivel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            CambiarNivel(indiceNivel);
+        }
+        if (pasarNivel)
+        {
+            CambiarNivel(indiceNivel);
+        }
+    }
+
+    public void CambiarNivel(int indice)
+    {
+        SceneManager.LoadScene(indice);
+    }
+}
--- Assets/_Unity Essentials/Source Files/Lighting/Scripts/UpdateCollectibleCount.cs
+++ Assets/_Unity Essentials/Source Files/Lighting/Scripts/UpdateCollectibleCount.cs
@@ -4,8 +4,8 @@ using System; // Required for Type handling
 
 public class UpdateCollectibleCount : MonoBehaviour
 {
-    private TextMeshProUGUI collectibleText; // Reference to the TextMeshProUGUI component
-
+    public TextMeshProUGUI collectibleText; // Reference to the TextMeshProUGUI component
+    
     void Start()
     {
         collectibleText = GetComponent<TextMeshProUGUI>();
@@ -25,13 +25,14 @@ public class UpdateCollectibleCount : MonoBehaviour
     private void UpdateCollectibleDisplay()
     {
         int totalCollectibles = 0;
-
+        
         // Check and count objects of type Collectible
         Type collectibleType = Type.GetType("Collectible");
         if (collectibleType != null)
         {
             totalCollectibles += UnityEngine.Object.FindObjectsOfType(collectibleType).Length;
         }
+    
 
         // Optionally, check and count objects of type Collectible2D as well if needed
         Type collectible2DType = Type.GetType("Collectible2D");
@@ -45,4 +46,5 @@ public class UpdateCollectibleCount : MonoBehaviour
 
 
     }
+    
 }
